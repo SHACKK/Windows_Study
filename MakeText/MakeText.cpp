bbd3d668a345f0dadb09ddc6f6b8dd081ae2a3b0
SHACKK <jfhg456@kakao.com>
2022-03-16 19:18:34 +0900

    문자열 입력 샘플 코드

---
+++ MakeText/MakeText.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <conio.h>
+
+void AssembleHangul(std::wstring* Dest, wchar_t Src)
+{
+	*Dest = Src;
+}
+int main()
+{
+	while (true)
+	{
+		wchar_t Text = (wchar_t)_getch();
+		std::wstring NewText;
+		AssembleHangul(&NewText, Text);
+
+		wprintf(L"%s", NewText.c_str());
+	}
+	return 0;
+}
