c44a7a04b433caf46bfe94d319ddc1c40f9b0c0d
SHACKK <jfhg456@kakao.com>
2022-03-28 16:04:41 +0900

    PrintUnicode_Sample Code

--- MakeText/MakeText.cpp
+++ MakeText/MakeText.cpp
@@ -1,7 +1,8 @@
 ﻿#include <string>
 #include <conio.h>
+#include <Windows.h>
 
-void AssembleHangul(std::wstring* Dest, wchar_t Src)
+void AssembleHangul(std::wstring* Dest, char Src)
 {
 	*Dest = Src;
 }
@@ -9,11 +10,12 @@ int main()
 {
 	while (true)
 	{
-		wchar_t Text = (wchar_t)_getch();
+		int Text = (int)_getch();
 		std::wstring NewText;
 		AssembleHangul(&NewText, Text);
 
-		wprintf(L"%s", NewText.c_str());
+		//wprintf(L"%s", NewText.c_str());
+		printf("%x\n", Text);
 	}
 	return 0;
 }
