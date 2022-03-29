48814fb45e678d2b5ec90f4c3d70fc2f00f9606d
SHACKK <jfhg456@kakao.com>
2022-03-29 20:15:19 +0900

    초성 중성 종성 선언

--- MakeText/MakeText.cpp
+++ MakeText/MakeText.cpp
@@ -2,20 +2,38 @@
 #include <conio.h>
 #include <Windows.h>
 
-void AssembleHangul(std::wstring* Dest, char Src)
+const char choseong[] = { 'r','R','s','e','E','f','a','q','Q','t','T','d','w','W','c','z','x','v','g' };
+const char jungseong[] = { 'k','o','i','O','j,','p','u','P','h','hk','ho','hl','y','n','nj','np','nl','b','m','ml','l' };
+const char jongseong[] = { 'r','R','rt','s','sw','sg','e','f','fr','fa','fq','ft','fx','fv','fg','a','q','qt','t','T','d','w','c','z','x','v','g' };
+
+std::wstring AssembleHangul(std::wstring strCurrentContext, char c)
 {
-	*Dest = Src;
+	if ('\n' == c)
+		return strCurrentContext;
+
+	if ('\b' == c)
+		return strCurrentContext = strCurrentContext.substr(0, strCurrentContext.length() - 1);
+
+
+	std::wstring strRet = strCurrentContext;
+	strRet.push_back(c);
+	return strRet;
 }
+
 int main()
 {
+	std::wstring strContext;
 	while (true)
 	{
-		int Text = (int)_getch();
-		std::wstring NewText;
-		AssembleHangul(&NewText, Text);
+		char c = (char)_getch();
+		std::wstring strNewContext = AssembleHangul(strContext, c);
 
-		//wprintf(L"%s", NewText.c_str());
-		printf("%x\n", Text);
+		if (strNewContext == strContext)
+			continue;
+
+		strContext = strNewContext;
+
+		wprintf(L"%s\n", strContext.c_str());
 	}
 	return 0;
 }
