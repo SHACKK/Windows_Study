a2a7a53e9773b9ee0640bdb442c6dc0f345a2af4
SHACKK <jfhg456@kakao.com>
2022-04-01 00:09:04 +0900

    살려줘

--- MakeText/MakeText.cpp
+++ MakeText/MakeText.cpp
@@ -1,14 +1,13 @@
 ﻿#include <string>
 #include <conio.h>
-#include <Windows.h>
+#include <cstring>
 
-const char choseong[] = { 'r','R','s','e','E','f','a','q','Q','t','T','d','w','W','c','z','x','v','g' };
-const char jungseong[] = { 'k','o','i','O','j,','p','u','P','h','hk','ho','hl','y','n','nj','np','nl','b','m','ml','l' };
-const char jongseong[] = { 'r','R','rt','s','sw','sg','e','f','fr','fa','fq','ft','fx','fv','fg','a','q','qt','t','T','d','w','c','z','x','v','g' };
+const char* choseong[] = { "r","R","s","e","E","f","a","q","Q","t","T","d","w","W","c","z","x","v","g" };
+const char* jungseong[] = { "k","o","i","O","j,","p","u","P","h","hk","ho","hl","y","n","nj","np","nl","b","m","ml","l" };
+const char* jongseong[] = { "r","R","rt","s","sw","sg","e","f","fr","fa","fq","ft","fx","fv","fg","a","q","qt","t","T","d","w","c","z","x","v","g" };
 
 struct Completed_Eumjeol
 {
-	int type;
 	int choseong;
 	int jungseong;
 	int jongseong;
@@ -19,37 +18,69 @@ int FindUnicode(Completed_Eumjeol stsyllable)
 	return 44032 + ((stsyllable.choseong * 21) + stsyllable.jungseong) * 28 + stsyllable.jongseong;
 }
 
-void FindIndex(char c)
+int FindIndex(const char* chararray[], char* c)
 {
-
+	// 인덱스 번호 반환
+	return 1;
 }
 
-void InsertChar(Completed_Eumjeol &stsyllable, char c)
+bool SortChar(char* c)
 {
-
+	for (int i = 0; i < sizeof(choseong); i++)
+	{
+		if (c == choseong[i])
+			return false;
+	}
+	return true;
 }
 
-std::wstring AssembleHangul(std::wstring strCurrentContext, char c)
+void InsertChar(Completed_Eumjeol &stsyllabel, char* c)
 {
-	if ('\n' == c)
+	bool flag = SortChar(c); // 1이면 모음, 0이면 자음
+
+	//초성 삽입
+	if (stsyllabel.choseong == NULL)
+		stsyllabel.choseong = FindIndex(choseong, c);
+	// 중성 삽입
+	else if ((stsyllabel.choseong != NULL && stsyllabel.jungseong == NULL))
+		stsyllabel.jungseong = FindIndex(jungseong, c);
+	// 중성이 두개인 경우 재설정 후 삽입
+	else if (stsyllabel.choseong != NULL && stsyllabel.jungseong != NULL && flag == true)
+	{
+		char* tmp_char = (char*)jungseong[stsyllabel.jungseong]; // 액세스 오류
+		strncat_s(tmp_char, 1, c, 1);
+		stsyllabel.jungseong = FindIndex(jungseong, tmp_char);
+	}	
+	// 종성 삽입
+	else if (stsyllabel.choseong !=NULL && stsyllabel.jungseong != NULL && stsyllabel.jongseong == NULL && flag == false)
+		stsyllabel.jongseong = FindIndex(jongseong, c);
+	// 종성이 두개인 경우 재설정 후 삽입
+}
+
+std::wstring AssembleHangul(std::wstring strCurrentContext, Completed_Eumjeol &stsyllabel, char* c)
+{
+	if ("\n" == c)
 		return strCurrentContext;
 
-	if ('\b' == c)
+	if ("\b" == c)
 		return strCurrentContext = strCurrentContext.substr(0, strCurrentContext.length() - 1);
 
+	InsertChar(stsyllabel, c);
 
 	std::wstring strRet = strCurrentContext;
-	strRet.push_back(c);
+	strRet.push_back(FindUnicode(stsyllabel));
 	return strRet;
 }
 
 int main()
 {
 	std::wstring strContext;
+
+	Completed_Eumjeol stsyllable;
 	while (true)
 	{
 		char c = (char)_getch();
-		std::wstring strNewContext = AssembleHangul(strContext, c);
+		std::wstring strNewContext = AssembleHangul(strContext, stsyllable, &c);
 
 		if (strNewContext == strContext)
 			continue;
