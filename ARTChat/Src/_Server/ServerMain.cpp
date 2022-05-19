b22dc7b646e8ce9d3c6daa0dc4338358646acc8b
SHACKK <jfhg456@kakao.com>
2022-05-19 19:41:25 +0900

    ServerMain

---
+++ ARTChat/Src/_Server/ServerMain.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#pragma comment(lib, "Ws2_32.lib")
+
+#define PORT 55000
+#define MAX_CONNECTION 20
+
+int main()
+{
+	ST_WSA_INITIALIZER stWsaInit;
+
+	std::vector<CConnectionSuper*> vecConnectionPool;
+
+	for (int i = 0; i < MAX_CONNECTION; i++)
+	{
+		CChatConnection* connection = new CChatConnection;
+		vecConnectionPool.push_back(connection);
+	}
+
+	ST_SERVER_INIT stServerInit(PORT,vecConnectionPool);
+	CServer server;
+	server.StartUp(stServerInit);
+
+	do
+	{
+		Sleep(1000);
+	} while(true);
+
+	server.ShutDown();
+	
+	return 0;
+}
---
+++ ARTChat/Src/_Server/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+#include <Windows.h>
+#include <string>
+#include <vector>
+
+#include "../SocketServer/_SocketServer.h"
