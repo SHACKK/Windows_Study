1d253b54004575741a5d8351aa4100e458afa2ee
SHACKK <jfhg456@kakao.com>
2022-05-31 19:16:05 +0900

    CChatClient.Send() 테스트 구현

--- ARTChat/Src/Tester/main.cpp
+++ ARTChat/Src/Tester/main.cpp
@@ -4,42 +4,102 @@
 #define PORT 56000
 #define MAX_CONNECTION 1
 
-
-void CheckRet(int nRet)
+struct ST_ACCEPTTHREAD_PARAM
 {
-	if (nRet == 1)
-		printf("[SUCCESS] Server StartUp()\n");
-	else
+	CServer* server;
+	CChatConnection* connection;
+};
+
+DWORD WINAPI AcceptThread(LPVOID pContext)
+{
+	ST_ACCEPTTHREAD_PARAM& stParam = *(ST_ACCEPTTHREAD_PARAM*)pContext;
+	sockaddr_in service;
+	service.sin_family = AF_INET;
+	service.sin_addr.s_addr = INADDR_ANY;
+	service.sin_port = htons(PORT);
+	int nExcep = ::bind(stParam.server->GetListenSocket(), (sockaddr*)&service, sizeof(service));
+	if (SOCKET_ERROR == nExcep) throw std::exception("Failed to Bind");
+
+	nExcep = listen(stParam.server->GetListenSocket(), 200);
+	if (SOCKET_ERROR == nExcep) throw std::exception("Failed to Listen");
+
+	sockaddr RemoteInfo;
+	int nRemoteInfoSize = (int)sizeof(RemoteInfo);
+	SOCKET hConnectionSocket = ::accept(stParam.server->GetListenSocket(), &RemoteInfo, &nRemoteInfoSize);
+
+	stParam.connection->SetSocket(hConnectionSocket, stParam.server);
+
+	return 0;
+}
+
+DWORD WINAPI ConnectThread(LPVOID pContext)
+{
+	CChatClient& client = *(CChatClient*)pContext;
+	ST_SERVER_INFO stServerInfo;
+	stServerInfo.IP = "127.0.0.1";
+	stServerInfo.Port = 56000;
+	client.Connect(stServerInfo);
+
+	return 0;
+}
+
+void MakeConnection(CServer* server, CChatConnection* connection, CChatClient* client)
+{
+	try
 	{
-		printf("[FAILED] Server StartUp()\n");
-		printf("[ERROR CODE] %d\n", WSAGetLastError());
+		ST_ACCEPTTHREAD_PARAM stArg;
+		stArg.server = server;
+		stArg.connection = connection;
+		HANDLE hAcceptThread = ::CreateThread(nullptr, 0, AcceptThread, &stArg, 0, nullptr);
+		HANDLE hConnectThread = ::CreateThread(nullptr, 0, ConnectThread, client, 0, nullptr);
+
+		::WaitForSingleObject(hAcceptThread, INFINITE);
+		::WaitForSingleObject(hConnectThread, INFINITE);
 	}
+	catch (std::exception& e)
+	{
+		printf("%s", e.what());
+	}
+	
+}
+
+bool SendTest()
+{
+	try 
+	{
+		CServer server;
+		CChatConnection connection;
+		CChatClient client;
+		MakeConnection(&server, &connection, &client);
+
+		std::wstring strSendMessage = L"TEST";
+		client.Send(strSendMessage);
+		std::wstring strRecvMessage = connection.Recv();
+		return true;
+	}
+	catch (...)
+	{
+		printf("[ERROR] %d", WSAGetLastError());
+		return false;
+	}
+}
+
+void CheckResult(bool bRes, std::string strTestName)
+{
+	if (bRes)
+		printf("[SUCCESS] %s", strTestName.c_str());
+	else
+		printf("[FAILED] %s", strTestName.c_str());
 }
 
 int main()
 {
 	std::setlocale(LC_ALL, "ko_KR.UTF-8");
-	int nRet;
-
-	// 1.Server.StartUp()
 	ST_WSA_INITIALIZER stWsaInit;
-	CServer server;
-	WORD nPort = 56000;
-	std::vector<CConnectionSuper*> vecConnectionPool;
-	for (int i = 0; i < MAX_CONNECTION; i++)
-	{
-		CChatConnection* connection = new CChatConnection;
-		vecConnectionPool.push_back(connection);
-	}
-	ST_SERVER_INIT stServerInit(PORT, vecConnectionPool);
-	nRet = server.StartUp(stServerInit);
-	if (nRet == 0)
-		printf("[SUCCESS] Server StartUp()\n");
-	else
-	{
-		printf("[FAILED] Server StartUp()\n");
-		printf("[ERROR CODE] %d\n", WSAGetLastError());
-	}
+	bool bRes;
+
+	bRes = SendTest();
+	CheckResult(bRes, "SendTest");
 
 	return 0;
 }
