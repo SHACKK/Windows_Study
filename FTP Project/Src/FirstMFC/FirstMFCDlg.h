065a1286a674d81cc3083695ea6ef5c813affcca
SHACKK <jfhg456@kakao.com>
2022-06-08 16:12:43 +0900

    MFC Project

---
+++ FTP Project/Src/FirstMFC/FirstMFCDlg.h
@@ -0,0 +1,50 @@
+﻿
+// FirstMFCDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CFirstMFCDlg 대화 상자
+class CFirstMFCDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CFirstMFCDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FIRSTMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnTvnSelchangedTree1(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnDeltaposSpin1(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnIpnFieldchangedIpaddress1(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnTcnSelchangeTab1(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnBnClickedCheck2();
+	afx_msg void OnBnClickedCheck3();
+	afx_msg void OnBnClickedCheck4();
+	afx_msg void OnBnClickedCheck7();
+	afx_msg void OnBnClickedCheck12();
+	afx_msg void OnBnClickedCheck25();
+	afx_msg void OnBnClickedCheck21();
+	afx_msg void OnLbnSelchangeList7();
+	afx_msg void OnBnClickedCheck10();
+	afx_msg void OnLbnSelchangeList6();
+};
---
+++ FTP Project/Src/FirstMFC/resource.h
@@ -0,0 +1,51 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// FirstMFC.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FIRSTMFC_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDR_TOOLBAR1                    129
+#define IDC_LIST6                       1021
+#define IDC_LIST7                       1022
+#define IDC_LIST8                       1023
+#define IDC_CHECK2                      1032
+#define IDC_CHECK3                      1033
+#define IDC_CHECK4                      1034
+#define IDC_CHECK5                      1035
+#define IDC_CHECK6                      1036
+#define IDC_CHECK7                      1037
+#define IDC_CHECK8                      1038
+#define IDC_CHECK10                     1040
+#define IDC_CHECK11                     1041
+#define IDC_CHECK12                     1042
+#define IDC_CHECK13                     1043
+#define IDC_CHECK14                     1044
+#define IDC_CHECK15                     1045
+#define IDC_CHECK16                     1046
+#define IDC_CHECK17                     1047
+#define IDC_CHECK18                     1048
+#define IDC_CHECK19                     1049
+#define IDC_CHECK20                     1050
+#define IDC_CHECK21                     1051
+#define IDC_CHECK22                     1052
+#define IDC_CHECK23                     1053
+#define IDC_CHECK24                     1054
+#define IDC_CHECK25                     1055
+#define IDC_CHECK26                     1056
+#define IDC_CHECK27                     1057
+#define IDC_CHECK28                     1058
+#define IDC_CHECK29                     1059
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1040
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
